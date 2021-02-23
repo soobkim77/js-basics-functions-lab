e21908369cc8fb4bd91e57ca5cc6aa5d8458e953
Soo-Beom <soobeomkim77@gmail.com>
2021-02-22 16:55:17 -0800

    commit tree 6ba62467d8b50e26b7d6120d66bf102d49d3e452

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solution in this file!
+// let hq = 42
+
+function distanceFromHqInBlocks (someValue) {
+    if (someValue > 42) {
+        return someValue-42
+    }
+
+    else {
+        return 42 - someValue
+    }
+}
+
+function distanceFromHqInFeet(someValue) {
+    if (someValue > 42) {
+        return (someValue-42) * 264
+    }
+
+    else {
+        return (42 - someValue) * 264
+    }
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start-destination) * 264 
+    }
+    if (start < destination) {
+        return (destination - start) * 264 
+    }
+}
+
+function calculatesFarePrice (start, destination) {
+    let feet = Math.abs(start-destination) * 264
+    if (feet <= 400) {
+        return 0
+    }
+    else {
+    if (feet > 400  && feet < 2000) {
+        return ((feet-400)*.02)
+    }
+
+    else if (feet > 2000 && feet < 2500) {
+        return 25
+    }
+
+    else (feet >= 2500); {
+        return "cannot travel that far"
+    }
+    }
+}
+
+     
